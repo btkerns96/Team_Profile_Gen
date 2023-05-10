64d642578885ea691e4242afc5f2a92eeee33cc1
BrandonKerns <btkerns96@gmail.com>
2023-05-10 11:28:10 -0700

    Added classes

--- index.js
+++ index.js
@@ -12,4 +12,28 @@ teamArray = [];
 
 
 
-function runApp () 
+function runApp () {
+
+    function createTeam () {
+      inquirer.prompt([{
+        type: "list",
+        message: "What type of employee would you like to add to your team?",
+        name: "addEmployeePrompt",
+        choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
+      }]).then(function (userInput) {
+        switch(userInput.addEmployeePrompt) {
+          case "Manager":
+            addManager();
+            break;
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+  
+          default:
+            htmlBuilder();
+        }
+      })
+}}
